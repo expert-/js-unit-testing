99925960825cee243c5d2aea4f3b86e6b47e643f
Ayush <ayush15may@gmail.com>
2015-12-16 23:19:45 +0530

    passing test on custom made function

---
+++ kar-jasm/src/Investment.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ayush on 12/16/2015.
+ */
+function Investment (stock) {
+    this.stock = stock;
+};
---
+++ kar-jasm/src/Stock.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ayush on 12/16/2015.
+ */
+function Stock () {};
