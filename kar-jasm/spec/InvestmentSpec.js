5783ce79a49fa9269f8e1fdf571856e1b4c686eb
Ayush <ayush15may@gmail.com>
2015-12-16 22:55:18 +0530

    starting investment example

---
+++ kar-jasm/spec/InvestmentSpec.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Ayush on 12/16/2015.
+ */
+describe("Investment", function() {
+    it("should be of a stock", function(){
+        expect(investment.stock).toBe(stock);
+    });
+});
