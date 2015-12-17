dc0ccb452c55bfddfeb80fff2c48b632fd3b53cd
Ayush <ayush15may@gmail.com>
2015-12-17 19:32:23 +0530

    testing an object

--- kar-jasm/src/Investment.js
+++ kar-jasm/src/Investment.js
@@ -1,6 +1,8 @@
 /**
  * Created by Ayush on 12/16/2015.
  */
-function Investment (stock) {
-    this.stock = stock;
+function Investment (params ) {
+    var params = params || {};
+    this.stock = params.stock;
+    this.shares = params.shares;
 };
